93d88779c95cbd5149b00f2b5e28988a561c4465
Claudia-teng <sydeng102@gmail.com>
2022-09-14 21:00:20 +0800

    style: order confirm page

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -60,52 +60,79 @@ function Order({ sessionId, orderConfirmInfo, ws, timer }) {
 
   return (
     <>
-      <p className={styles.text}>Total: {orderConfirmInfo.total}</p>
-      {orderConfirmInfo?.tickets?.map((ticket) => {
-        return (
-          <>
-            <p className={styles.text}>{ticket.title}</p>
-            <p className={styles.text}>{ticket.time}</p>
-            <p className={styles.text}>{ticket.venue}</p>
-            <p className={styles.text}>{ticket.area}</p>
-            <p className={styles.text}>{ticket.seatId}</p>
-            <p className={styles.text}>{ticket.row}</p>
-            <p className={styles.text}>{ticket.column}</p>
-            <p className={styles.text}>{ticket.price}</p>
-          </>
-        );
-      })}
-      <button onClick={(event) => onSubmitOrder(event)}>確認訂單</button>
-      <div className={styles.table}>
-        <Table>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>區域</th>
-              <th>座位</th>
-              <th>價錢</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td colSpan={2}>Larry the Bird</td>
-              <td>@twitter</td>
-            </tr>
-          </tbody>
-        </Table>
+      <div className={styles.orderContainer}>
+        <div className={styles.payment}>
+          <h1>付款方式</h1>
+          <p>ibon 取票付款</p>
+          <p>
+            訂單完成 20 分鐘至 1 小時間至全台 7-11 門市內的 ibon
+            機台自行列印付款單據，至臨櫃付款後並取票，逾時付款訂單自動取消， 亦不予保留。
+          </p>
+        </div>
+        <div className={styles.event}>
+          <h1>活動</h1>
+          <div>
+            <p>{orderConfirmInfo?.tickets[0].title}</p>
+          </div>
+        </div>
+        <div className={styles.dateVenue}>
+          <div>
+            <div>
+              <h1>日期</h1>
+              <div>
+                <p>{orderConfirmInfo?.tickets[0].time}</p>
+              </div>
+            </div>
+            <div>
+              <h1>場地</h1>
+              <div>
+                <p>{orderConfirmInfo?.tickets[0].venue}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <h1>訂單細節</h1>
+          <div className={styles.table}>
+            <Table>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>區域</th>
+                  <th>座位</th>
+                  <th>價錢</th>
+                </tr>
+              </thead>
+              <tbody>
+                {orderConfirmInfo?.tickets?.map((ticket, index) => {
+                  return (
+                    <>
+                      <tr>
+                        <td>{index + 1}</td>
+                        <td>{ticket.area}</td>
+                        <td>
+                          {ticket.row} 排 {ticket.column} 號
+                        </td>
+                        <td>{ticket.price}</td>
+                      </tr>
+                    </>
+                  );
+                })}
+              </tbody>
+            </Table>
+          </div>
+        </div>
+        <div className={styles.summary}>
+          <div>
+            <span>總張數：</span> <span>{orderConfirmInfo?.tickets.length}張</span>
+          </div>
+          <div>
+            <span>總價：</span> <span>${orderConfirmInfo?.total}張</span>
+          </div>
+        </div>
+        <div className={styles.button}>
+          <button onClick={(event) => onSubmitOrder(event)}>確認訂單</button>
+        </div>
       </div>
     </>
   );
