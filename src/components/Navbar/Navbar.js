4e2a038758dc4bdd6473d8de4806770a9d589ceb
Claudia-teng <sydeng102@gmail.com>
2022-10-12 21:35:03 +0800

    feat: edit logo text

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -15,7 +15,7 @@ function Navbar({ isLogin, setIsLogin }) {
     <>
       <div className={styles.navber}>
         <Link className={styles.logo} to="/">
-          TICKETCLUBBB
+          TICKETCLUB
         </Link>
         {isLogin ? (
           <Link className={styles.member} to="/profile">
